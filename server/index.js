301b96a7b5d8eead6710a727151d71520e175f71
denpoly <denpoly@gmail.com>
2022-04-12 21:02:10 -0700

    test 7

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const roomData = [
 app.get('/', (req, res) => {
   res.sendFile(__dirname +"/index.html")
 });
-//Test Change5
+//Test Change6
 
 io.on('connection', (socket) => {
   socket.on("action", function (action) {
