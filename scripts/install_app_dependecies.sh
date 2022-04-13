12dcb24995154d57e6053ec8939436b315ef8749
denpoly <denpoly@gmail.com>
2022-04-12 19:44:30 -0700

    adding CICD scripts

---
+++ scripts/install_app_dependecies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ec2-user/Friendle.io/my-app
+npm install
+cd ../server
+npm install
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/Friendle.io/my-app
+npm start
+npm run server
