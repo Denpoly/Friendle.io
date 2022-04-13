b01dd42e78f099a55a7f70a0af9912f922495eb2
denpoly <denpoly@gmail.com>
2022-04-13 09:51:09 -0700

    change scripts paths

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/Friendle.io/my-app
+cd ../my-app
 npm install
 npm install express
 cd ../server
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/Friendle.iog/my-app
+cd ../my-app
 npm start
 npm run server
