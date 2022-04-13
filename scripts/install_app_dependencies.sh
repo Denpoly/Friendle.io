7c7cfb2bd9be8c8e0a3bab028f1e25b1bae7ee26
denpoly <denpoly@gmail.com>
2022-04-13 12:12:20 -0700

    teststtsst

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -2,8 +2,8 @@
 #install node
 yum install curl -y
 --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
-yum install nodejs -y
-yum install npm -y
+yum install nodejs -y --skip-broken
+yum install npm -y --skip-broken
 
 cd /home/ec2-user/Friendle.io/my-app
 npm install
