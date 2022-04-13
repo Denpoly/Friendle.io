8b20592df9807267890273657c9cdb8b0ad2aa2f
denpoly <denpoly@gmail.com>
2022-04-13 11:07:54 -0700

    changes please work

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads 
 cd /home/ec2-user/Friendle.io/my-app
 npm start
 npm run server
