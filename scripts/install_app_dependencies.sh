2a120773d44db4d41038bf84bf63dbbcd8c81ca2
denpoly <denpoly@gmail.com>
2022-04-25 14:02:19 -0700

    trying a new version of node

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -2,14 +2,15 @@
 #install node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 14 -g
-nvm use 14
+nvm install 18 -g
+nvm use 18
 cd /home/ec2-user/Friendle.io/my-app
 npm install
 npm install express
 cd /home/ec2-user/Friendle.io/server
 npm install
 cd /home/ec2-user/Friendle.io/my-app
+pm2 stop all
 pm2 start ../my-app/startServScript.js
 pm2 start ../my-app/startscript.js
 # nohup npm start
