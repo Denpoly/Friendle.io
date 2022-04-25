d6ecb14a19687ae4f1487a93f9e5717e596d7e4c
denpoly <denpoly@gmail.com>
2022-04-25 13:52:47 -0700

    nvm changes

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,14 +3,14 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 14 -g
-
+nvm use 14
 cd /home/ec2-user/Friendle.io/my-app
 npm install
 npm install express
 cd /home/ec2-user/Friendle.io/server
 npm install
 cd /home/ec2-user/Friendle.io/my-app
-pm2 start ../my-app/startservscript.js
+pm2 start ../my-app/startServScript.js
 pm2 start ../my-app/startscript.js
 # nohup npm start
 # nohup npm run server
