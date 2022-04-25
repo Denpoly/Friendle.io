d64922aec2422059745f6e05728dd7a686e8624e
denpoly <denpoly@gmail.com>
2022-04-25 12:55:28 -0700

    changes to install_app_dependencies

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -2,12 +2,11 @@
 #install node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install v10.13.0 -g
 
 cd /home/ec2-user/Friendle.io/my-app
 npm install
 npm install express
-npm install pm2
 cd /home/ec2-user/Friendle.io/server
 npm install
 cd /home/ec2-user/Friendle.io/my-app
